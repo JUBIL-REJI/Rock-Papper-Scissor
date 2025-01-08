12e4cccaddc83b6cd8368611fa2171cc387758b5
jubil reji <rejijubil@gmail.com>
2025-01-08 23:55:47 +0530

    added page not found componet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {  Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home'
 import Game from './elements/game/Game'
+import NotFound from './NotFound'
 
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
 
      <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='game' element={<Game/>}/>
+      <Route path='/game' element={<Game/>}/>
+      <Route path='*' element={<NotFound/>}/>
      </Routes>
   
   )
---
+++ src/NotFound.jsx
@@ -0,0 +1,14 @@
+
+
+function NotFound() {
+  return (
+    <div>
+      <h1 className="text-4xl flex justify-center items-center">
+      NotFound
+      </h1>
+    
+    </div>
+  )
+}
+
+export default NotFound
